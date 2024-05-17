1e2729b4b163b05218a9b53bcd1ed06a0dd2651d
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-05-16 21:32:38 -0300

    Vetores

--- exercicios/ExercicioVetores_AB.java
+++ exercicios/ExercicioVetores_AB.java
@@ -18,6 +18,32 @@ public class ExercicioVetores_AB {
         for (int i = 0; i < B.length; i++) {
             System.out.print(B[i] + " ");
         }
+
+        // exibindo vetor A
+        System.out.println("\nVetor A");
+        for(int i = 0; i < A.length; i++){
+            System.out.print(A[i] + "  ");
+        }
+        System.out.println();
+
+        VetoresMultiplicados();
+
+    }
+
+    static void VetoresMultiplicados(){
+
+        System.out.println("Vetores Multiplicados");
+        int[] A = {1,2,3,4,5,6,7,8};
+        int[] B = new int[A.length]; // passando o mesmo tamanho para B
+
+        for(int i = 0; i < A.length; i++){
+            B[i] = A[i] * 2;
+            System.out.print("Vetor A: " + A[i] + " ");
+
+            System.out.println("Vetor B: " + B[i]);
+        }
     }
 }
+
+
 
