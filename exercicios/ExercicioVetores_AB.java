da1fa10abf5cf33247ab2632f06b8e822e8b7b85
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2024-05-16 21:11:12 -0300

    att

--- exercicios/ExercicioVetores_AB.java
+++ exercicios/ExercicioVetores_AB.java
@@ -2,7 +2,22 @@ package exercicios;
 
 public class ExercicioVetores_AB {
     public static void main(String[] args) {
+        // Criando o vetor A com 5 elementos inteiros
+        int[] A = {10, 20, 30, 40, 50};
 
+        // Criando o vetor B com o mesmo tamanho que A
+        int[] B = new int[A.length];
 
+        // Copiando os elementos de A para B
+        for (int i = 0; i < A.length; i++) {
+            B[i] = A[i];
+        }
+
+        // Exibindo os elementos de B
+        System.out.println("Vetor B (mesmos elementos de A):");
+        for (int i = 0; i < B.length; i++) {
+            System.out.print(B[i] + " ");
+        }
     }
 }
+
